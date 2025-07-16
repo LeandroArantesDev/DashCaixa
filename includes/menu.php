e5e4212c975a1b832f7748eb38d4fea6e3e37a66
oCrazyoff <walyssonribeiro3@gmail.com>
2025-07-16 15:49:13 -0300

    feat: inicio do formulario de login

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,9 @@
-<aside>
-    <nav>
-        <a href="<?= BASE_URL ?>">Inicio</a>
-        <a href="<?= BASE_URL ?>/produtos">Produtos</a>
-        <a href="#">Usuarios</a>
-    </nav>
-</aside>
+<?php if (isset($_SESSION)): ?>
+    <aside>
+        <nav>
+            <a href="<?= BASE_URL ?>">Inicio</a>
+            <a href="<?= BASE_URL ?>/produtos">Produtos</a>
+            <a href="#">Usuarios</a>
+        </nav>
+    </aside>
+<?php endif ?>
--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
-$titulo = "Inicio";
-$css = "index";
+$titulo = "Login";
+$css = "formulario";
 include("includes/inicio.php")
 ?>
-<div class="conteudo">
-    <h1>Teste</h1>
+<div class="flex items-center justify-center w-full h-dvh bg-sky-100">
+    <form class="flex items-center justify-center flex-col rounded-xl p-5 bg-white shadow-2xs"
+        action="backend/auth/login.php" method="POST">
+        <img src="assets/img/logo.png" alt="Logo DashCaixa" class="w-45 h-45">
+        <h1>Login</h1>
+        <p>Sistema de Gestão de Vendas</p>
+        <div class="input-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Seu email" required>
+        </div>
+        <div class="input-group">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" placeholder="Sua senha" required>
+        </div>
+        <button type="submit">Entrar</button>
+        <a href="#">Esqueceu sua senha?</a>
+    </form>
 </div>
 <?php include("includes/fim.php") ?>
