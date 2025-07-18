920d4f49d586004d83cd097ef363fdc08f75a41c
oCrazyoff <walyssonribeiro3@gmail.com>
2025-07-18 14:18:35 -0300

    feat: inicio da estilização do formulario de login

--- includes/inicio.php
+++ includes/inicio.php
@@ -15,7 +15,8 @@ date_default_timezone_set('America/Sao_Paulo');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/<?= htmlspecialchars($css) ?>.css">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title><?= htmlspecialchars($titulo ?? "DashCaixa") ?></title>
--- includes/menu.php
+++ includes/menu.php
@@ -1,14 +1,12 @@
-<?php //if (isset($_SESSION)): 
-?>
-<aside>
-    <nav>
-        <a href="<?= BASE_URL ?>pages/dashboard" target="_self">Dashboard</a>
-        <a href="<?= BASE_URL ?>pages/vendas" target="_self">Vendas</a>
-        <a href="<?= BASE_URL ?>pages/produtos" target="_self">Produtos</a>
-        <a href="<?= BASE_URL ?>pages/categorias" target="_self">Categorias</a>
-        <a href="<?= BASE_URL ?>pages/usuarios" target="_self">Usuarios</a>
-        <a href="<?= BASE_URL ?>pages/historico" target="_self">Histórico de Vendas</a>
-    </nav>
-</aside>
-<?php //endif 
-?>
+<?php if (isset($_SESSION)): ?>
+    <aside>
+        <nav>
+            <a href="<?= BASE_URL ?>pages/dashboard" target="_self">Dashboard</a>
+            <a href="<?= BASE_URL ?>pages/vendas" target="_self">Vendas</a>
+            <a href="<?= BASE_URL ?>pages/produtos" target="_self">Produtos</a>
+            <a href="<?= BASE_URL ?>pages/categorias" target="_self">Categorias</a>
+            <a href="<?= BASE_URL ?>pages/usuarios" target="_self">Usuarios</a>
+            <a href="<?= BASE_URL ?>pages/historico" target="_self">Histórico de Vendas</a>
+        </nav>
+    </aside>
+<?php endif ?>
