f338becbaf16d2aed2e4a1f36f41b2c333f4a915
RuanParreira <ruan.parreira@edu.uniube.br>
2025-07-29 22:13:11 -0300

    fix: ajustes gerais

--- pages/vendas/index.php
+++ pages/vendas/index.php
@@ -9,14 +9,13 @@ include("../../includes/inicio.php")
             <p>Realize vendas e gere fichas</p>
         </div>
     </div>
-    <div class="flex gap-3 items-center justify-between h-[90%] w-full">
+    <div class="flex gap-3 items-center justify-between h-[86%]">
         <div class="tabela-form w-4/6 bg-white h-full">
-            <h2 class="text-xl font-semibold text-gray-900 p-6">Buscar produtos</h2>
-            <div class="flex w-full border-b border-[var(--cinza-borda)] gap-4 p-4 bg-white -mt-5">
+            <div class="flex w-full border-b border-[var(--cinza-borda)] gap-4 p-4 bg-white">
                 <input class="input-filtro col-span-2 w-full p-2 border border-[var(--cinza-borda)] rounded-lg"
                     type="text" name="busca" id="busca" placeholder="Buscar produto por nome...">
             </div>
-            <div class="max-h-[90%] overflow-y-auto">
+            <div class="h-full flex-1 overflow-y-auto py-2">
                 <div class="h-max flex flex-col gap-3 p-3 pb-20">
                     <?php
                     // buscando todos os produtos disponiveis
@@ -67,7 +66,7 @@ include("../../includes/inicio.php")
                             </div>
                         <?php endwhile ?>
                     <?php else: ?>
-                        <a class="flex items-center justify-center gap-2 w-full border border-[var(--cinza-borda)] p-2 rounded-lg" href="../produtos">Cadastrar produtos <i class="bi bi-plus-lg bg-blue-200 rounded-lg text-blue-600 text-lg flex justify-center items-center w-8 h-8"></i></a>
+                        <a class="flex items-center justify-center gap-2 w-full border border-[var(--cinza-borda)] p-2 rounded-lg" href="../produtos">Cadastrar produtos <i class="bi bi-plus-lg bg-blue-200 rounded-lg text-sky-600 text-lg flex justify-center items-center w-8 h-8"></i></a>
                     <?php endif ?>
                 </div>
             </div>
@@ -76,24 +75,19 @@ include("../../includes/inicio.php")
             id="carrinho-lateral">
             <div class="p-6 border-b border-gray-200 flex items-center justify-between">
                 <div class="flex space-x-3">
-                    <i class="bi bi-cart text-xl text-blue-600"></i>
+                    <i class="bi bi-cart text-xl text-sky-600"></i>
                     <h2 class="text-xl font-semibold text-gray-900">Carrinho</h2>
                 </div>
                 <div>
-                    <button type="button" id="limpar-carrinho" title="Limpar carrinho" class="hidden cursor-pointer">
-                        <span class="font-semibold text-red-700 hover:text-red-800 transition-colors">
-                            Limpar Carrinho
-                            <i class="bi bi-trash3 text-xl"></i>
-                        </span>
-
+                    <button type="button" id="limpar-carrinho" title="Limpar carrinho" class="hidden cursor-pointer text-red-700 hover:text-red-800 transition-colors">
+                        <i class="bi bi-trash3 text-xl"></i>
                     </button>
                 </div>
-
             </div>
-            <ul id="lista-carrinho" class="text-center text-gray-500 py-12 overflow-x-auto flex-1 p-6 space-y-4">
-                <li class="carrinho-vazio text-center text-gray-500 py-12">
+            <ul id="lista-carrinho" class="text-center text-gray-500 py-4 overflow-x-auto flex-1 px-6 space-y-2">
+                <li class="text-center text-gray-500 items-center">
                     <i class="bi bi-cart text-6xl mx-auto text-gray-300"></i>
-                    <p class="text-lg font-medium mt-10">Nenhum item adicionado</p>
+                    <p class="text-lg font-medium mt-5">Nenhum item adicionado</p>
                     <p>Busque e adicione produtos à ficha</p>
                 </li>
             </ul>
@@ -103,17 +97,17 @@ include("../../includes/inicio.php")
                 <input type="hidden" name="usuario_id" id="usuario_id" value="<?= $_SESSION["id"] ?>">
                 <!-- Aba Total  -->
                 <div class="p-6 border-t border-gray-200">
-                    <div class="bg-blue-50 rounded-lg p-4 mb-4">
+                    <div class="bg-blue-50 rounded-lg py-2 px-4 mb-4">
                         <div class="flex justify-between items-center">
-                            <span class="text-xl font-medium text-gray-900">TOTAL:</span>
-                            <span id="valor-total" class="text-3xl font-bold text-blue-600">R$ 0,00</span>
+                            <span class="text-lg font-medium text-gray-900">TOTAL:</span>
+                            <span id="valor-total" class="text-xl font-bold text-sky-600">R$ 0,00</span>
                         </div>
                         <div class="mt-2 text-sm text-gray-600">
                             <span id="itens-total">0 itens</span>
                         </div>
                     </div>
                     <button type="submit" id="btn-finalizar"
-                        class="w-full bg-blue-600 text-white py-4 rounded-lg font-semibold text-xl hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors disabled:opacity-50 disabled:cursor-not-allowed shadow-lg cursor-pointer"
+                        class="w-full bg-sky-600 text-white py-2 rounded-lg font-semibold text-xl hover:bg-sky-700 focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 transition-colors disabled:opacity-50 disabled:cursor-not-allowed shadow-lg cursor-pointer"
                         disabled>Finalizar</button>
                 </div>
             </form>
