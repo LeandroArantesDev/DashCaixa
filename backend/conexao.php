fcdb14b712d1a5be3a2744b22f818fc02b9e78b9
oCrazyoff <walyssonribeiro3@gmail.com>
2025-08-11 20:12:25 -0300

    fix: conexao com o banco de dados automatico

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,6 +1,18 @@
 <?php
 
-$conexao = mysqli_connect("localhost", "root", "", "dash_caixa");
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'dashcaixa';
+} else {
+    $host = 'sql313.infinityfree.com';
+    $username = 'if0_39683322';
+    $password = 'UikYkRLYHWEjOp';
+    $dbname = 'if0_39683322_dashcaixa';
+}
+
+$conexao = new mysqli($host, $username, $password, $dbname);
 
 if ($conexao->connect_error) {
     die("Erro na conexão!" . $conexao->connect_error);
